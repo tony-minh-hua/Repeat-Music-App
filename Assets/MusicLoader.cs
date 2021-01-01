01a70075d7b257af40a62f2ae37a758f40c47175
tony-minh-hua <63430512+tony-minh-hua@users.noreply.github.com>
2020-12-31 17:07:44 -0800

    Initial commit

---
+++ Assets/MusicLoader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Linq;
+
+public class MusicLoader : MonoBehaviour
+{
+    private string musicFile;
+
+    public Text ActiveSongText;
+    public GameObject Panel;
+    public GameObject ButtonPrefab;
+
+    private void Start()
+    {
+        ReadFileNames();
+    }
+
+    private void ReadFileNames ()
+    {
+        DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath);
+        FileInfo[] info = dir.GetFiles("*.ogg");
+        info.Select(f => f.FullName).ToArray();
+
+        foreach (FileInfo f in info)
+        {
+            Debug.Log(f.Name);
+            GameObject instance = Instantiate(ButtonPrefab);
+            instance.transform.SetParent(Panel.transform);
+            instance.GetComponentInChildren<Text>().text = f.Name;
+            instance.GetComponentInChildren<Button>().onClick.AddListener(delegate { SetMusicFile(f.Name); }) ;
+        }
+    }
+
+    private void SetMusicFile (string name)
+    {
+        musicFile = name;
+        ActiveSongText.text = musicFile;
+        StartCoroutine(LoadMusic());
+    }
+
+    public static string GetFileLocation(string relativePath) {
+        return "file://" + Path.Combine(Application.streamingAssetsPath, relativePath);
+    }
+
+    IEnumerator LoadMusic ()
+    {
+        WWW www = new WWW(GetFileLocation(musicFile));
+        MusicPlayer.Song = www.GetAudioClip();
+        yield return new WaitUntil(() => MusicPlayer.Song.isReadyToPlay);
+    }
+    
+}
---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public static AudioClip Song;
+    public AudioSource SourceAudio;
+    public InputField NumberPlaysInput;
+    public Text NumberPlaysText;
+    public int numberOfPlays;
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        Screen.fullScreen = false;
+        NumberPlaysText.text = "Number of Plays: " + numberOfPlays.ToString();
+    }
+
+    IEnumerator PlaySong ()
+    {
+        for (int i = 0; i < numberOfPlays; i++)
+        {
+            SourceAudio.PlayOneShot(Song);
+            yield return new WaitForSeconds(Song.length);
+        }
+        Application.Quit();
+    }
+
+    public void NumberOfPlaysInput()
+    {
+        numberOfPlays = int.Parse(NumberPlaysInput.text);
+        NumberPlaysText.text = "Number of Plays: " + numberOfPlays.ToString();
+    }
+
+    public void PlayButton()
+    {
+        StartCoroutine(PlaySong());
+    }
+
+    public void QuitAppButton()
+    {
+        Application.Quit();
+    }
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("Screenmanager Resolution Width", 800);
+        PlayerPrefs.SetInt("Screenmanager Resolution Height", 600);
+        PlayerPrefs.SetInt("Screenmanager Is Fullscreen mode", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
